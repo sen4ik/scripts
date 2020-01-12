af1b0f6dc6873ad6c7e9fbc4c21be429692e8a0c
Artur Sentsov <sen4ik@gmail.com>
2020-01-11 16:34:38 -0800

    gitlab runner added

---
+++ gitlab/runner_install.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+# https://docs.gitlab.com/runner/install/linux-repository.html
+# install gitlab runner
+curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | sudo bash
+sudo apt-get install gitlab-runner
+
+# https://docs.gitlab.com/runner/register/index.html
+# register runner
+sudo gitlab-runner register
+
+# add to sudoers
+echo "gitlab-runner ALL=(ALL) NOPASSWD: /bin/ln, /bin/chmod" | sudo tee -a /etc/sudoers
+
+# service gitlab-runner start
