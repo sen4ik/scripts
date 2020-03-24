8f6f064d4c9fa4cea9feabac216ec69ec541d756
Artur Sentsov <sen4ik@gmail.com>
2020-03-24 00:28:53 -0700

    debian/apt_my_box.sh

--- debian/apt_my_box.sh
+++ debian/apt_my_box.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y findutils sudo gnupg bash htop nano vim net-tools curl wget rsync openssh-server ca-certificates tmux locate git tree dirmngr unzip apt-transport-https ca-certificates gnupg2 software-properties-common
-# update db for locate 
-sudo updatedb
+sudo apt install -y findutils lsof sudo gnupg bash htop nano vim net-tools curl wget rsync openssh-server ca-certificates tmux git tree dirmngr unzip apt-transport-https ca-certificates gnupg2 software-properties-common
+
+read -p "Do you want to install locate (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        sudo apt install -y locate
+		# update db for locate 
+		sudo updatedb
+    ;;
+    * )
+        echo ""
+    ;;
+esac
