91b3c388f0256bffcdb049e63c060efb11ef0dfb
Artur Sentsov <sen4ik@gmail.com>
2020-03-28 19:09:03 -0700

    shells

---
+++ debian/_purge_mysql.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read -p "Are you sure you want to remove mysql and all its configs(y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        sudo apt-get remove --purge mysql-server mysql-client mysql-common -y
+		sudo apt-get autoremove -y
+		sudo apt-get autoclean
+		rm -rf /etc/mysql
+		sudo find / -iname 'mysql*' -exec rm -rf {} \;
+    ;;
+    * )
+        echo ""
+    ;;
+esac
