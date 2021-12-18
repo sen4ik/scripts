42b2e3c616c043dbd55af0e888e3061cf866d7dd
Artur Sentsov <sen4ik@gmail.com>
2021-12-17 23:58:22 -0800

    upd

---
+++ gitlab/backups_location.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo cat /etc/gitlab/gitlab.rb | grep backup_path
--- rsync/copy.sh
+++ rsync/copy.sh
@@ -5,8 +5,10 @@ BACKUP_TO=/media/WD1TB/MyFiles/Backups
 LOGSDIR=$BACKUP_TO/logs
 
 sudo rsync --perms --recursive --times --verbose --progress \
---itemize-changes --log-file=$LOGSDIR/rsync-$(date +%Y-%m-%d).txt --log-file-format="%i %f %b" \
+--itemize-changes --log-file=$LOGSDIR/rsync-$(date +%Y-%m-%d-%H-%M-%S).txt --log-file-format="%i %f %b" \
 --copy-links $BACKUP_FROM $BACKUP_TO
+
+# sudo rsync --perms --recursive --times --verbose --progress --itemize-changes --log-file=/mnt/external/logs/rsync-$(date +%Y-%m-%d).txt --log-file-format="%i %f %b" --copy-links /mnt/Vol1/MyFiles/Music/Uncategorized /mnt/external/Music
 
 # verify
 # rsync -avhc $BACKUP_FROM $BACKUP_TO
