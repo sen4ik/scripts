a0935414899829e0ab1aed9b6b086fa703a72958
Artur Sentsov <sen4ik@gmail.com>
2022-05-03 23:02:30 -0500

    upd

--- macos/brew_my_mac.sh
+++ macos/brew_my_mac.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
-brew install bat,ccat,nano,awk,p7zip,htop,maven,git,wget,nmap,tmux,mc,exa
+
+brew install python@3.10
+
+brew install openjdk@8
+
+brew install --cask grammarly google-chrome firefox vlc keepassxc tunnelblick audacity balenaetcher istat-menus vnc-viewer
+
+brew install --cask intellij-idea sublime-text postman dbeaver-community docker iterm2
+
+brew install --cask slack telegram viber discord
+
+brew install nano awk htop wget curl nmap tmux watch tree maven ccat bat
+
+brew install git
+
+brew install youtube-dl
+
+brew install p7zip exa
+
+brew install --cask wsjtx gridtracker fldigi chirp
+
+# brew install libreoffice
+# brew install ffmpeg
+# brew install findutils coreutils
+
+brew install allure
