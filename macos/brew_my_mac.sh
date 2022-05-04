dff4cbb219e0cb85ccf1da413659bb27a07a4be3
Artur Sentsov <sen4ik@gmail.com>
2022-05-03 23:28:37 -0500

    upd

--- macos/brew_my_mac.sh
+++ macos/brew_my_mac.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+xcode-select --install
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew analytics off
+# export HOMEBREW_NO_ANALYTICS=1
+
 brew install python@3.10
 
 brew install openjdk@8
 
 brew install --cask grammarly google-chrome firefox vlc keepassxc tunnelblick audacity balenaetcher istat-menus vnc-viewer
 
-brew install --cask intellij-idea sublime-text postman dbeaver-community docker iterm2
+brew install --cask intellij-idea sublime-text postman dbeaver-community docker iterm2 figma
 
 brew install --cask slack telegram viber discord
+
+brew install --cask kindle google-drive
 
 brew install nano awk htop wget curl nmap tmux watch tree maven ccat bat
 
